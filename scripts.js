e88a15774d47a93be451385cc9a3d0e5b9a041a5
Denniskaninu <denniskaninu254@gmail.com>
2024-07-05 17:06:29 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,13 +18,25 @@ function showSection(sectionId) {
 }
 
 function newQuote() {
-    var quotes = [
-        "\"Act as if what you do makes a difference. It does.\" - William James",
-        "\"Success is not final, failure is not fatal: It is the courage to continue that counts.\" - Winston Churchill",
-        "\"You have within you right now, everything you need to deal with whatever the world can throw at you.\" - Brian Tracy",
-        "\"Believe you can and you're halfway there.\" - Theodore Roosevelt",
-        "\"Don't watch the clock; do what it does. Keep going.\" - Sam Levenson"
-    ];
+      var quotes = [
+    "\"Act as if what you do makes a difference. It does.\" - William James",
+    "\"Success is not final, failure is not fatal: It is the courage to continue that counts.\" - Winston Churchill",
+    "\"You have within you right now, everything you need to deal with whatever the world can throw at you.\" - Brian Tracy",
+    "\"Believe you can and you're halfway there.\" - Theodore Roosevelt",
+    "\"Don't watch the clock; do what it does. Keep going.\" - Sam Levenson",
+    "\"The only limit to our realization of tomorrow will be our doubts of today.\" - Franklin D. Roosevelt",
+    "\"You are never too old to set another goal or to dream a new dream.\" - C.S. Lewis",
+    "\"Success usually comes to those who are too busy to be looking for it.\" - Henry David Thoreau",
+    "\"The best way to predict the future is to create it.\" - Abraham Lincoln",
+    "\"The future belongs to those who believe in the beauty of their dreams.\" - Eleanor Roosevelt",
+    "\"Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.\" - Roy T. Bennett",
+    "\"Opportunities don't happen. You create them.\" - Chris Grosser",
+    "\"Your life does not get better by chance, it gets better by change.\" - Jim Rohn",
+    "\"The only way to do great work is to love what you do.\" - Steve Jobs",
+    "\"The way to get started is to quit talking and begin doing.\" - Walt Disney",
+    "\"It always seems impossible until it's done.\" - Nelson Mandela"
+];
+
     var randomIndex = Math.floor(Math.random() * quotes.length);
     document.getElementById('motivational-quote').innerText = quotes[randomIndex];
 }
